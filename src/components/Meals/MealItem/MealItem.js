a2aac31dfbb1f616df1a4ed833495cb3a8ba57f4
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-02-26 02:29:40 +0530

    adding a form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
     return ( <
@@ -9,6 +10,11 @@ const MealItem = props => {
         h3 > { props.name } < /h3> <
         div className = { classes.description } > { props.description } < /div> <
         div className = { classes.price } > { price } < /div> <
+        /div> <
+        div >
+        <
+        MealItemForm / >
+        <
         /div> <
         /li>
     )
--- src/components/Meals/MealSummary.js
+++ src/components/Meals/MealSummary.js
@@ -3,7 +3,7 @@ const MealSummary = () => {
     return ( <
         section className = { classes.summary } >
         <
-        h1 > Delicious Food, Delivered To Yo < /h1> <
+        h1 > Delicious Food, Delivered To You < /h1> <
         p >
         Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home. <
         /p> <
