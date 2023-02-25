92c794d56a01c9d38611d56e44841a34c096abca
Abhishekkanojiya3 <abhi67562@gmail.com>
2023-02-26 01:26:51 +0530

    making item visible

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,52 @@
 import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 const DUMMY_MEALS = [{
         id: 'm1',
         name: 'Chicken Tikka',
-        description: 'Finest fish and veggies',
+        description: 'Traditional Indian Cuisine',
         price: 22.99,
     },
     {
         id: 'm2',
         name: 'Dal Makhni',
-        description: 'A german specialty!',
+        description: 'A indian specialty!',
         price: 16.5,
     },
     {
         id: 'm3',
         name: 'Butter Chicken',
-        description: 'American, raw, meaty',
+        description: 'Indian, Buttery, meaty',
         price: 12.99,
     },
     {
         id: 'm4',
         name: 'Chicken Biryani',
-        description: 'Healthy...and green...',
+        description: 'Tasty...and delicious...',
         price: 18.99,
     },
 ];
 
 const AvailableMeals = () => {
-        const mealList = DUMMY_MEALS.map((meal) => < li > { meal.name } < /li>);
+    const mealList = DUMMY_MEALS.map((meal) =>
+        <
+        MealItem key = { meal.id }
+        name = { meal.name }
+        description = { meal.description }
+        price = { meal.price }
+        />
+    );
 
-            return ( <
-                section className = { classes.meals } >
-                <
-                ul > { mealList } <
-                /ul> <
-                /section>
-            );
+    return ( <
+        section className = { classes.meals } >
+        <
+        Card >
+        <
+        ul > { mealList } <
+        /ul> <
+        /Card> <
+        /section>
+    );
 
-        }
-        export default AvailableMeals;
+}
+export default AvailableMeals;
